535b907fa8bb34f69e4124aeeb7b5dbc209e3347
grumblewoes <culturuse@gmail.com>
2023-11-29 21:48:11 +0100

    forgot logic is on cs server :/

--- src/main/java/via/sep4/sep4test/database/repository/FavoriteRepository.java
+++ src/main/java/via/sep4/sep4test/database/repository/FavoriteRepository.java
@@ -3,13 +3,8 @@ package via.sep4.sep4test.database.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import via.sep4.sep4test.database.domain.DomainFavorite;
-import via.sep4.sep4test.database.domain.DomainItem;
-
-import java.util.List;
-
 
 @Repository
 public interface FavoriteRepository extends JpaRepository<DomainFavorite, Integer> {
     DomainFavorite findByUserIdAndItemId(int userId, int itemId);
-    List<DomainFavorite> findAllByUserId(int userId);
 }
--- src/main/java/via/sep4/sep4test/database/repository/ItemRepository.java
+++ src/main/java/via/sep4/sep4test/database/repository/ItemRepository.java
@@ -4,10 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import via.sep4.sep4test.database.domain.DomainItem;
 
-import java.util.List;
-
 @Repository
 public interface ItemRepository extends JpaRepository<DomainItem, Integer>
 {
-    List<DomainItem> findAllByIdIn(List<Integer> itemIds);
 }
