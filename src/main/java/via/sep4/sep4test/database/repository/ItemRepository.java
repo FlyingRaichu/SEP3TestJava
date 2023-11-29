72165b8ed99b34f0f48ab8cf3ea5f81f9f4956bf
grumblewoes <culturuse@gmail.com>
2023-11-29 19:57:33 +0100

    added ViewFavorites, edited favorites.proto and ItemRepository

--- src/main/java/via/sep4/sep4test/database/repository/ItemRepository.java
+++ src/main/java/via/sep4/sep4test/database/repository/ItemRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import via.sep4.sep4test.database.domain.DomainItem;
 
+import java.util.List;
+
 @Repository
 public interface ItemRepository extends JpaRepository<DomainItem, Integer>
 {
+    List<DomainItem> findAllByIdIn(List<Integer> itemIds);
 }
--- src/main/java/via/sep4/sep4test/services/FavoriteService.java
+++ src/main/java/via/sep4/sep4test/services/FavoriteService.java
@@ -1,22 +1,34 @@
 package via.sep4.sep4test.services;
 import com.google.protobuf.Empty;
+import com.google.protobuf.Int32Value;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import via.sep4.protobuf.Favorite;
 import via.sep4.protobuf.FavoriteServiceGrpc;
+import via.sep4.protobuf.FavoritesRequest;
+import via.sep4.protobuf.Item;
 import via.sep4.sep4test.database.domain.DomainFavorite;
+import via.sep4.sep4test.database.domain.DomainItem;
 import via.sep4.sep4test.database.repository.FavoriteRepository;
+import via.sep4.sep4test.database.repository.ItemRepository;
 import via.sep4.sep4test.mappers.FavoriteMapper;
+import via.sep4.sep4test.mappers.ItemMapper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @GrpcService
 public class FavoriteService extends FavoriteServiceGrpc.FavoriteServiceImplBase
 {
     private FavoriteRepository favRepository;
+    private ItemRepository itemRepository;
     private final FavoriteMapper mapper = FavoriteMapper.INSTANCE;
+    private final ItemMapper itemMapper = ItemMapper.INSTANCE;
 
-    public FavoriteService(FavoriteRepository favRepository)
+    public FavoriteService(FavoriteRepository favRepository, ItemRepository itemRepository)
     {
         this.favRepository = favRepository;
+        this.itemRepository = itemRepository;
     }
 
     @Override public void getFavorite(Favorite favorite, StreamObserver<Favorite> responseObserver)
@@ -43,6 +55,26 @@ public class FavoriteService extends FavoriteServiceGrpc.FavoriteServiceImplBase
         favRepository.delete(fav);
 
         responseObserver.onNext(Empty.newBuilder().build());
+        responseObserver.onCompleted();
+    }
+
+    @Override public void getUserFavorites(FavoritesRequest request, StreamObserver<Item> responseObserver) {
+        List<DomainFavorite> userFavorites = favRepository.findAllByUserId(request.getUserId());
+        List<Integer> ids = new ArrayList<>();
+
+        //grabs ids of each item from favorites
+        for (DomainFavorite d : userFavorites)
+        {
+            ids.add(d.getItemId());
+        }
+
+        //get the associated item and convert to proto obj
+        List<DomainItem> items = itemRepository.findAllByIdIn(ids);
+        for (DomainItem i : items)
+        {
+            responseObserver.onNext(itemMapper.toProto(i));
+        }
+
         responseObserver.onCompleted();
     }
 }
