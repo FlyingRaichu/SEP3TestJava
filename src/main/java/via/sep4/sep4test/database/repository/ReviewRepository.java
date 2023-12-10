4aad982fba3b78bff5eb65820d737bf973b99374
FlyingRaichu <lyubomir.veselinov@gmail.com>
2023-12-10 13:42:59 +0100

    DOmainOrder userid

--- src/main/java/via/sep4/sep4test/database/repository/ReviewRepository.java
+++ src/main/java/via/sep4/sep4test/database/repository/ReviewRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<DomainReview, Integer> {
     List<DomainReview> findDomainReviewByItem(DomainItem domainItem);
+    List<DomainReview> findDomainReviewsByItem(DomainItem id);
 }
